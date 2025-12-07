90aa70af5d2de70aa9c80560e56f22899a8c2b09
Rajid Ansari <rajidansari25@gmail.com>
2025-12-07 19:56:42 +0530

    added blog page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 
 export default function BlogsPage() {
   return (
-	<div className="h-auto w-full bg-white px-[1rem] sm:px-[5rem] md:px-[7rem] lg:px-[20rem] transition-all">
-            <div className="min-h-screen border-outside">
-                <h1 className={`text-9xl`}>Blogs</h1>
+	<div className="h-auto w-full bg-white px-[1rem] sm:px-[5rem] md:px-[7rem] lg:px-[20rem] transition-all padding-main">
+            <div className="h-[12rem] border-outside py-16">
+                <h1 className={`text-8xl text-gray-600`}>Blogs</h1>
             </div>
-            <div className="min-h-screen border-outside">
-                <h1 className={`text-9xl`}>Blogs</h1>
-            </div>
-            <div className="min-h-screen border-outside">
-                <h1 className={`text-9xl`}>Blogs</h1>
+            <div className='h-[33rem] flex items-center justify-center text-gray-600 w-full border-right border-left'>
+                <h2>Working on this.</h2>
             </div>
         </div>
   )
